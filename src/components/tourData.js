e1b11b39d4b58a562f5f7dc9531637fb4fb6c4e2
Steven Creighton <ste.creighton@googlemail.com>
2019-02-01 15:30:10 +0000

    All Images

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -15,18 +15,57 @@ export const tourData = [
   },
   {
     id: 3,
-    city: "london",
-    img: "./img/london.jpeg",
-    name: "london royal palace tour",
+    city: "York",
+    img: "./img/york.jpg",
+    name: "York Car Trip",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "We did everything here, I drove a Ferrari dead fast, we went in a corn maze and saw a pig race among other things!"
   },
   {
     id: 4,
-    city: "tokyo",
-    img: "./img/tokyo.jpeg",
-    name: "tokyo sushi tour",
+    city: "Edinburgh",
+    img: "./img/edinburgh.jpg",
+    name: "Haggis, Scotch Eggs and Deep Fried Mars Bars",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Another trip where we went on loads of adventures. Didn't get any fried chocolate though :D"
+  },
+  {
+    id: 5,
+    city: "Berlin",
+    img: "./img/berlin.jpg",
+    name: "Kiss Your Wife!",
+    info:
+      "We went on the bus tour all over the city, we saw all the sites and went to the zoo!"
+  },
+  {
+    id: 6,
+    city: "Rhodes",
+    img: "./img/rhodes.jpg",
+    name: "We got so brown!",
+    info:
+      "We went to Lindoses, had all the chill, got all the sunburn then I got all the itchy!"
+  },
+  {
+    id: 7,
+    city: "Barcelona",
+    img: "./img/barcelona.jpg",
+    name: "Viva Espana!",
+    info: "We went to Spains and I got dead excited about Gaudi!"
+  },
+  {
+    id: 8,
+    city: "Krakow",
+    img: "./img/krakow.jpg",
+    name: "Polski trip",
+    info:
+      "We went to Poland, went in the salt mines, went the castle and then I had a big sausage!"
+  },
+  {
+    id: 9,
+    city: "Tucepi",
+    img: "./img/tucepi.jpg",
+    name: "Croatias",
+    info:
+      "Croatia was lovely, but we had to hide from the big storm and all the rains! We must go back for the suns!"
   }
 ];
